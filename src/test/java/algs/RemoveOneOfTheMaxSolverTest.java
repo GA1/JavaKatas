d174856623c610715f32ed67f0ff9ee42f53d4df
GA1 <nvkga1@gmail.com>
2017-01-27 20:39:14 +0100

    Add one max solver test

---
+++ src/test/java/algs/RemoveOneOfTheMaxSolverTest.java
@@ -0,0 +1,54 @@
+package algs;
+
+import org.junit.Test;
+
+import java.util.LinkedList;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class RemoveOneOfTheMaxSolverTest {
+
+
+    private RemoveOneOfTheMaxSolver solver = new RemoveOneOfTheMaxSolver();
+    private LinkedList<Integer> nums = new LinkedList<Integer>();
+    private LinkedList<Integer> result = new LinkedList<Integer>();
+
+
+    @Test
+    public void solve1() {
+        nums.add(1);
+        result = solver.removeOneOfTheMax(nums);
+        assertEquals(0, result.size());
+    }
+
+    @Test
+    public void solve5_7() {
+        nums.add(5);
+        nums.add(7);
+        result = solver.removeOneOfTheMax(nums);
+        assertEquals(1, result.size());
+        assertEquals(5, (int)result.get(0));
+    }
+
+    @Test
+    public void solve7_7() {
+        nums.add(7);
+        nums.add(7);
+        result = solver.removeOneOfTheMax(nums);
+        assertEquals(1, result.size());
+        assertEquals(7, (int)result.get(0));
+    }
+
+    @Test
+    public void solve7_5_7() {
+        nums.add(7);
+        nums.add(5);
+        nums.add(7);
+        result = solver.removeOneOfTheMax(nums);
+        assertEquals(2, result.size());
+        assertTrue(result.indexOf(5) != -1);
+        assertTrue(result.indexOf(7) != -1);
+    }
+
+}
