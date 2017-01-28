0e2226fcb5e0f1b4d2b56e947f337c26debb4d89
GA1 <nvkga1@gmail.com>
2017-01-28 18:54:09 +0100

    Add test for remove point to minimize area solver

---
+++ src/test/java/algs/removePointSolverTest.java
@@ -0,0 +1,28 @@
+package algs;
+
+import org.junit.Test;
+
+import java.math.BigInteger;
+
+import static org.junit.Assert.assertEquals;
+
+public class removePointSolverTest {
+
+//    private removePointSolver solver = new removePointSolver();
+//    private removePointSolver.Point p1, p2, p3, p4, result;
+//
+//    @Test
+//    public void points4() {
+//        p1 = new removePointSolver.Point(0, -100, 1);
+//        p2 = new removePointSolver.Point(0, 2, 1);
+//        p3 = new removePointSolver.Point(0, 2, 2);
+//        p4 = new removePointSolver.Point(0, 1, 2);
+//        result = solver.solve(new removePointSolver.Point[]{p1, p2, p3, p4});
+//        assertEquals(2, result.x);
+//        assertEquals(3, result.y);
+//    }
+//
+//
+//
+
+}
